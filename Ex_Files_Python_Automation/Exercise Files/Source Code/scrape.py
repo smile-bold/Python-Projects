d6a220ecd70443e45c815932aa0d77e66d1f4065
smile-bold <mcbolding@my.milligan.edu>
2021-02-12 16:29:40 -0800

    How to filter retrieved URL content (scraping), then paginate it

--- Ex_Files_Python_Automation/Exercise Files/Source Code/scrape.py
+++ Ex_Files_Python_Automation/Exercise Files/Source Code/scrape.py
@@ -1,14 +1,14 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
-url = 'http://quotes.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text,'lxml')
-quotes = soup.find_all("span", class_="text")
-authors = soup.find_all("small", class_="author")
-tags = soup.find_all("div", class_="tags")
-for i in range(0,len(quotes)):
+url = 'http://quotes.toscrape.com/'\
+response = requests.get(url) #retrieves url content
+soup = BeautifulSoup(response.text, 'lxml') #converts html to lxml format
+quotes = soup.find_all('span', class='text') #finds only html content with span element and class of "text"
+authors = soup.find_all('small', class="author")
+tags = soup.find_all('div', class="tags")
+for i in range(0, len(quotes));
     print(quotes[i].text)
     print(authors[i].text)
-    quoteTags = tags[i].find_all('a',class_='tag')
+    quoteTags = tags[i].find_all('a', class_='tag')
     for quoteTag in quoteTags:
         print(quoteTag.text)
--- Ex_Files_Python_Automation/Exercise Files/Source Code/scrapePages.py
+++ Ex_Files_Python_Automation/Exercise Files/Source Code/scrapePages.py
@@ -2,24 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 url = 'https://scrapingclub.com/exercise/list_basic/'
 count = 1
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
-for i in items:
-    itemName = i.find('h4', class_='card-title').text.strip('\n')
+response = requests.get(url) #grab url data
+soup = BeautifulSoup(response.text, 'lxml') #parse into lxml
+items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4') #stores outer layer of data in variable
+for i in items: #loop through data store titles and price for each product. 
+    itemName = i.find('h4', class_='card-title').text.strip('\n') # function expression that removes character or something from data
     itemPrice = i.find('h5').text
     print('%s) Price: %s , Item Name: %s' % (count, itemPrice, itemName))
     count = count + 1
-pagination = soup.find('ul', class_='pagination')
-pages = pagination.find_all('a', class_='page-link')
+pagination = soup.find('ul', class_='pagination') #finds unordered list with class of "pagination"
+pages = pagination.find_all('a', class_='page-link') #finds all links with class "page-link"
 urls = []
 for page in pages:
     pageNum = int(page.text) if page.text.isdigit() else None
     if pageNum != None:
-        link = page.get('href')
-        urls.append(link)
+        link = page.get('href') #retrieves hyperlink reference
+        urls.append(link) #Adds page number onto url element in array
 for i in urls:
-    response = requests.get(url + i)
+    response = requests.get(url + i) # Adds already retrieved url content into a new scrape of url content then loops through it
     soup = BeautifulSoup(response.text, 'lxml')
     items = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
     for i in items:
