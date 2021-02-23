023b9f316b3771c0803506cb6eccbcd24562c944
smile-bold <mcbolding@my.milligan.edu>
2021-02-22 16:54:42 -0800

    Using selenium ; partial

--- Ex_Files_Python_Automation/Exercise Files/Source Code/webBrowsing.py
+++ Ex_Files_Python_Automation/Exercise Files/Source Code/webBrowsing.py
@@ -1,14 +1,14 @@
-from selenium import webdriver
-url = 'https://www.seleniumeasy.com/test/basic-first-form-demo.html'
-driver = webdriver.Chrome()
-driver.get(url)
-messageField = driver.find_element_by_xpath('//*[@id="user-message"]')
+from selenium import webdriver  # use webdriver package
+
+driver = webdriver.Chrome()  # declare driver variable as certain driver.
+driver.get(
+    "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
+)  # find webpage to automate
+messageField = driver.find_element_by_xpath('//*[@id="user-message"]')  #
 messageField.send_keys("Hello World")
 showMessageButton = driver.find_element_by_xpath('//*[@id="get-input"]/button')
 showMessageButton.click()
-additionField1 = driver.find_element_by_xpath('//*[@id="sum1"]')
-additionField1.send_keys('10')
-additionField2 = driver.find_element_by_xpath('//*[@id="sum2"]')
-additionField2.send_keys('12')
-getTotalButton = driver.find_element_by_xpath('//*[@id="gettotal"]/button')
-getTotalButton.click()
+additionField1 = driver.find_element_by_xpath("")
+additionField1.send_keys("10")
+additionField2 = driver.find_element_by_xpath("")
+additionField2.send_keys("11")
